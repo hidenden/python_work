67235d6b343ee4ea52d7602f8cc751e5a1dbcef3
Hidenari <hidenari.miwa@mac.com>
2023-10-29 17:38:32 +0900

    Add -p, -d option by argparse

--- gpx/gpxbydate.py
+++ gpx/gpxbydate.py
@@ -7,27 +7,33 @@ import sys
 import gpxpy
 import gpxpy.gpx
 from datetime import datetime, timezone
+import argparse
+import os
 
-def showhow():
-    print("gpbydate <gpx-file> [<gpx-file>...]")
-    sys.exit(1)
+def valid_dir(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"Directory {path} does not exist.")
 
 def gpxbydate_main():
-    if len(sys.argv) == 1:
-        showhow()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--prefix", help='prefix for output gpx files.', default="out_")
+    parser.add_argument("-d", "--dir", help='output directory.', default=".", type=valid_dir)
+    parser.add_argument("gpxfiles", help='input gpx files', nargs="+")
+    args = parser.parse_args()
 
-    arg_files = sys.argv[1:]
     point_dir = {}
-    for arg_file in arg_files:
+    for arg_file in args.gpxfiles:
         parse_a_gpx(arg_file, point_dir)
 
-    out_gpxfiles(point_dir)
+    out_gpxfiles(point_dir, args.dir, args.prefix)
 
-def out_gpxfiles(pdir:dict[str, list]):
+def out_gpxfiles(pdir:dict[str, list], savedir:str, prefix:str):
     for date_str in pdir.keys():
-        out_a_gpxfile(date_str, pdir[date_str])
+        out_a_gpxfile(date_str, pdir[date_str], savedir, prefix)
 
-def out_a_gpxfile(date_str:str, points:list):
+def out_a_gpxfile(date_str:str, points:list, savedir:str, prefix:str):
     # Create a new GPX file
     gpx = gpxpy.gpx.GPX()
 
@@ -42,12 +48,12 @@ def out_a_gpxfile(date_str:str, points:list):
 
     # Sort the points
     points.sort(key=lambda x: x.time)
-    
+
     # Set all points
     for p in points:
         gpx_segment.points.append(p)
 
-    new_file_name = f"out_{date_str}.gpx"
+    new_file_name = f"{savedir}/{prefix}{date_str}.gpx"
     print(f"new file:{new_file_name}")
     with open(new_file_name, "w", encoding="utf-8") as file:
         file.write(gpx.to_xml())
@@ -60,7 +66,6 @@ def parse_a_gpx(fname:str, point_dir:dict[str, list]):
         for track in gpx.tracks:
             for segment in track.segments:
                 for point in segment.points:
-#                    print('Point at ({}, {}) -> {}'.format(point.latitude, point.longitude, point.elevation))
                     x = point.time.astimezone().strftime('%Y-%m-%d')
                     if not(x in point_dir):
                         point_dir[x] = []
